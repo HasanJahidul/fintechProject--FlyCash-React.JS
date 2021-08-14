5017b093369e65be0de5aac6ffd441742ca6ac67
HasanJahidul <joyaiub1@gmail.com>
2021-08-14 06:52:13 +0600

    pdf designing

--- flycash-react/src/components/customer/transactions/StatementPdfDownload.jsx
+++ flycash-react/src/components/customer/transactions/StatementPdfDownload.jsx
@@ -1,20 +1,16 @@
 import React, { useEffect, useState } from "react";
 import "../../../App.css";
 import StatementsTableRow from "./StatementsTableRow";
-import logo from "../../../black/img/flycash.png";
+import logo from "../../../black/img/flycash.png"
 import moment from "moment";
 
 const StatementPdfDownload = () => {
-  let email = "Joy#gmail.com";
+  let email = "Joy#gmail.com"
   let today = new Date();
-
-  let date =
-    today.getDate() +
-    "-" +
-    parseInt(today.getMonth() + 1) +
-    "-" +
-    today.getFullYear();
-
+  
+let date=today.getDate() + "-"+ parseInt(today.getMonth()+1) +"-"+today.getFullYear();
+  
+ 
   const getTransactionList = () => {
     fetch("http://localhost:8000/api/customer/transactionlist").then(
       (response) => {
@@ -34,29 +30,42 @@ const StatementPdfDownload = () => {
       }
     );
   }, []);
-  const print = () => {
+  const print =()=>{
     window.print();
-  };
+  }
   return (
-    <div>
-      <div className="content">
-        <div className="row photo">
-          <div className="col-md-12" >
-            Print Date: {date}
-            </div>
-        </div>
-        <div className="row">
-          <img className="photo" src={logo}></img>
-        </div>
-        <div class="row" style={{ right: "500px" }}>
+    
+    <div className= "pdf">
+      <div className= "content">
+        
+        <div className='row'>
+          
+        <div className ="details">
+                <h3> Print Date :{date}</h3>
+
+                <h4>Name :Jahidul Hasan</h4>
+                <h4>Email :joy@gmail.com</h4>
+                <h4>Phone :01631124587</h4>
+
+              </div>
+        <img className='photo' src={logo}></img>
+           </div>
+        <div class="row" style={{right: "500px"}}>
+          
           <div class="col-md-12">
+            
             <div class="card ">
+              
               <div class="card-header">
+              <button onClick={() => window.print()} align="center" type="submit" class="btn btn-fill btn-primary"> Print</button>
+              
                 <h3 class="card-title">Statement</h3>
+                
+               
               </div>
               <div class="card-body">
                 <div class="table-responsive-lg">
-                  <table class="table tablesorter " id="">
+                  <table class="table tablesorter ">
                     <thead class=" text-primary">
                       <tr>
                         <th>Account Number</th>
@@ -73,20 +82,15 @@ const StatementPdfDownload = () => {
                   </table>
                 </div>
               </div>
-              <button
-                onClick={() => window.print()}
-                align="center"
-                type="submit"
-                class="btn btn-fill btn-primary"
-              >
-                {" "}
-                Print
-              </button>
+              
             </div>
           </div>
+          
         </div>
+        
       </div>
-    </div>
+      </div>
+      
   );
 };
 export default StatementPdfDownload;
