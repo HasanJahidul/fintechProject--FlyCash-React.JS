0edd0ddfa2c6864903e2db461362647f0ec4e2db
HasanJahidul <joyaiub1@gmail.com>
2021-08-16 12:26:53 +0600

    officers merge solving

--- flycash-laravel/routes/api.php
+++ flycash-laravel/routes/api.php
@@ -7,11 +7,14 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+
 Route::post('/users-login', 'LoginController@verify');
 
 //========================   CUSTOMER   ======================================
 
-Route::get('/customer/transactionlist', 'CustomerTransactionController@index');
 
-Route::post('/transaction', 'CustomerTransactionController@makeTransaction');
-Route::get('/transaction1', 'CustomerTransactionController@index1');
+Route::get('/customer/transactionlist','CustomerTransactionController@index');
+
+
+Route::post('/transaction','CustomerTransactionController@makeTransaction');
+Route::get('/transaction1','CustomerTransactionController@index1');
--- flycash-react/src/App.js
+++ flycash-react/src/App.js
@@ -4,94 +4,33 @@ import "./App.css";
 import "./black/css/nucleo-icons.css";
 import cashinPNG from "./black/img/icons/cashin.png";
 import cashoutPNG from "./black/img/icons/cashout.png";
-import donatePNG from "./black/img/icons/donate.png";
 import transferPNG from "./black/img/icons/money.png";
 import paymentPNG from "./black/img/icons/payment.png";
 import rechargePNG from "./black/img/icons/recharge.png";
 import sendPNG from "./black/img/icons/sendmoney.png";
+import donatePNG from "./black/img/icons/donate.png";
 import Login from "./components/auth/login";
 import Register from "./components/auth/register";
 import Dashboard from "./components/customer/Dashboard";
 import Password from "./components/customer/Password";
 import Profile from "./components/customer/profile";
 import Addmoney from "./components/customer/transactions/addMoney";
-import Donate from "./components/customer/transactions/Donate";
 import TransactionList from "./components/customer/transactions/transactionlist";
 import ErrorPage from "./components/Error404";
-//=======================Start Officer Import File==============================
-import OfficerDashboard from "./components/officer/OfficerDashboard";
 import Welcome from "./components/welcome";
-import PrivateRoute from "./PrivateRoute";
-import PublicRoute from "./PublicRoute";
+import Donate from "./components/customer/transactions/Donate";
 
+//=======================Start Officer Import File==============================
+
+import OfficerDashboard from "./components/officer/OfficerDashboard";
+import PublicRoute from "./PublicRoute";
+import PrivateRoute from "./PrivateRoute";
 
 //=======================End Officer Import File================================
 
 function App() {
   //========================= CUSTOMER ===========================================
-
   
-  return (
-    <>
-      <Router>
-        <Switch>
-          <Route exact path="/customer/statement">
-            <TransactionList />
-          </Route>
-          <Route exact path="/">
-            <Welcome />
-          </Route>
-          <Route exact path="/register">
-            <Register />
-          </Route>
-          <Route exact path="/login">
-            <Login />
-          </Route>
-          <Route exact path="/customer-dashboard">
-            <Dashboard />
-          </Route>
-          <Route exact path="/customer-profile">
-            <Profile />
-          </Route>
-          <Route exact path="/customer-change-password">
-            <Password />
-          </Route>
-          <Route
-            exact
-            path="/customer/add-money"
-            children={<Addmoney status="Add Money" imgpath={cashinPNG}  buttonName="Add Money" numberType='Bank Account'/>}
-          ></Route>
-          <Route
-            exact
-            path="/customer/send-money"
-            children={<Addmoney status="Send Money" imgpath={sendPNG} buttonName="Send Money" numberType='FlyCash ' />}
-          ></Route>
-          <Route
-            exact
-            path="/customer/cash-out"
-            children={<Addmoney status="Cash out" imgpath={cashoutPNG}  buttonName="Cash out" numberType='Agent'/>}
-          ></Route>
-          <Route
-            exact
-            path="/customer/payment"
-            children={<Addmoney status="Payment" imgpath={paymentPNG}  buttonName="Pay" numberType='Merchant'/>}
-          ></Route>
-          <Route
-            exact
-            path="/customer/mobile-recharge"
-            children={<Addmoney status="Mobile Recharge" imgpath={rechargePNG}  buttonName="Recharge" numberType='Mobile'/>}
-          ></Route>
-          <Route
-            exact
-            path="/customer/transfer-money"
-            children={<Addmoney status="Transfer money" imgpath={transferPNG} buttonName="Transfer"  numberType='Bank Account'/>}
-          ></Route>
-          <Route
-            exact
-            path="/customer/donate"
-            children={<Donate status="Donation" imgpath={donatePNG} buttonName="Transfer"  numberType="Select NGO's"/>}
-          ></Route>
-
   return (
     <>
       <Router>
